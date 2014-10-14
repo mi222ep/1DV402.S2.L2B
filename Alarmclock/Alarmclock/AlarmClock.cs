eabc60cb65ba092a3d3d81a4d9fbb70f526be3d0
Matila Ingman <mi222ep@student.liu.se>
2014-10-14 11:10:45 +0200

    Klassen AlarmClock tillagd

---
+++ Alarmclock/Alarmclock/AlarmClock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    class AlarmClock
+    {
+        int AlarmHour { get; set; }
+        int AlarmMinute { get; set; }
+        int Hour { get; set; }
+        int Minute { get; set; }
+
+        //Konstruktor, måste vara tom, Anropar konstruktorn med 2 parametrar
+        public AlarmClock()
+        {
+
+        }
+        //Konstruktor, måste vara tom, Anropar konstruktorn med 4 parametrar
+        public AlarmClock(int hour, int minute)
+        {
+
+        }
+        //Detta är enda konstruktorn som får innehålla kod som leder till att fält i klassen tilldelas värden
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+
+        }
+        //Metod, dåe klockan att gå en minut, om ny tid stämmer överens med alarm, returnera TRUE, annars FALSE
+        public bool TickTock()
+        {
+            return false;
+        }
+        //Metod, retunerar tid + alarmtid, tex 8:42 (16:23)
+        public string ToString()
+        {
+            return ("Hej Hej");
+        }
+
+    }
+}
